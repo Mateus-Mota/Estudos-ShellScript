c28a23adc13850850049c61e4bf429c7e123a86d
Mateus <mateusmota0100@gmail.com>
2021-02-10 11:18:20 -0300

    Adicionando a resolução melhorada do exercício-02.

---
+++ Módulo 02 - Variaveis/atividades-repositorio/exercicio02-melhorado.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+read -p "Nome-1 = " nome1
+read -p "Nome-2 = " nome2
+read -p "Nome-3 = " nome3
+
+read -p "Fone-1 = " fone1
+read -p "Fone-2 = " fone2
+read -p "Fone-3 = " fone3
+
+echo "Fone-1 = ${fone1}, Fone-2 = ${fone2}, Fone-3 = ${fone3}"
+echo "Nome-1 = ${nome1}, Nome-2 = ${nome2}, Nome-3 = ${nome3}"
