bc4e851fcdb6e884c5a81db72823ff590887016c
Mateus_Mota <47959481+Mateus-Mota@users.noreply.github.com>
2021-03-02 18:58:42 -0300

    Update exercicio04.sh
    Removendo questão do exercicio04.sh

--- Módulo 01 - Introdução e Conceitos Basicos/atividades-disciplina/exercicio04.sh
+++ Módulo 01 - Introdução e Conceitos Basicos/atividades-disciplina/exercicio04.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
 
-#Melhore o script anterior para que este compacte o novo diretório criado, removendo este diretório após a sua compactação. (o arquivo compactado deve ser copiado para o diretório inicial, o diretório atual, no caso).
