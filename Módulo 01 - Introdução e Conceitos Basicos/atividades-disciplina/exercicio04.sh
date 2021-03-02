c56e696d70b51015e9cc47232f5db71091ab61dc
Mateus_Mota <47959481+Mateus-Mota@users.noreply.github.com>
2021-03-02 18:33:20 -0300

    Update exercicio04.sh
    Adicionando a atividade 04.

---
+++ Módulo 01 - Introdução e Conceitos Basicos/atividades-disciplina/exercicio04.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+#Melhore o script anterior para que este compacte o novo diretório criado, removendo este diretório após a sua compactação. (o arquivo compactado deve ser copiado para o diretório inicial, o diretório atual, no caso).
