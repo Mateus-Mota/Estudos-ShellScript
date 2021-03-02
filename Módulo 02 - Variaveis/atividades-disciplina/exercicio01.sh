913de3149248ec0b451928d3299737b050786c6c
Mateus <mateusmota0100@gmail.com>
2021-03-02 18:57:59 -0300

    .

---
+++ Módulo 02 - Variaveis/atividades-disciplina/exercicio01.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#Escreva um script que escreva uma frase motivacional
+
+echo "Coragem é a resistência ao medo e não a ausência dele."
---
+++ Módulo 02 - Variaveis/atividades-disciplina/exercicio02.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+#Escreva um script que pergunte ao usuário o caminho e liste todos os caminhos desses três diretórios.
