09f5be5c451922cd754fe12926899e713d55f2aa
Mateus_Mota <47959481+Mateus-Mota@users.noreply.github.com>
2021-03-16 15:00:04 -0300

    Update exercicio01.1.sh
    test

--- Módulo 05 - Redirecionadores e Conectores/atividades-disciplina/exercicio01.1.sh
+++ Módulo 05 - Redirecionadores e Conectores/atividades-disciplina/exercicio01.1.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+echo "'>' Sobrescreve o arquivo com a saida do comando anterior."
+echo "'>>' Adiciona o saida do comando anterior no final do arquivo."
+echo "'2>' Sobresceve o arquivo com a saida de erros"
+echo "'2>>' Adiciona a saida de erros ao final do arquivo "
+echo "'&>' Sobrescreve o arquivo com todas as saidas do comando anterior"
+echo "'&>>' Adiciona a todas as saidas do comando anterior no final do arquivo"
+echo "'<' Redireciona o arquivo para a entrada padrão do comando"
+echo "'<<' Redireciona a entrada padrão do comando para o documento escrito no bash"
+echo "'<<<' Redireciona a entrada padrão do comando para a string escrita no bash"
