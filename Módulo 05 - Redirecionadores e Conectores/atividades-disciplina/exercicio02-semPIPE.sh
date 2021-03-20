5cc0bf293f6b2388e017e7ae5a1940bf7bc5e6ee
Mateus <mateusmota0100@gmail.com>
2021-03-20 03:07:08 -0300

    Removido o lixo.

--- Módulo 05 - Redirecionadores e Conectores/atividades-disciplina/exercicio02-semPIPE.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-bc <<< $(cat a.txt)
--- Módulo 05 - Redirecionadores e Conectores/atividades-disciplina/exercicio03.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ls $1 1>> log.log; ls $1 2>> erro.log 
-ls $2 1>> log.log; ls $2 2>> erro.log
-ls $3 1>> log.log; ls $3 2>> erro.log
