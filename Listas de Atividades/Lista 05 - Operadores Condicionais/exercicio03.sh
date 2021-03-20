dd838a4f439603cc681b435bc05dbee1dc8adabf
Mateus <mateusmota0100@gmail.com>
2021-03-20 19:56:21 -0300

    Adicionado uma resolução melhorada da questão 03.

--- Listas de Atividades/Lista 05 - Operadores Condicionais/exercicio03.sh
+++ Listas de Atividades/Lista 05 - Operadores Condicionais/exercicio03.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 arq1=$(cat ${1} | wc -l)
 arq2=$(cat ${2} | wc -l)
-if [ $arq1 -gt $arq2 ];then 
-       	echo $2
-elif [ $arq2 -gt $arq1 ];then
-	echo $1
-fi
+(( $arq1 > $arq2 )) && echo $2
+(( $arq2 > $arq1 )) && echo $1
