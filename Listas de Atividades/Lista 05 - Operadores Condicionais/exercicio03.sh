efb914fa3995b71a98305cf2b9f2a68a13f7e104
Mateus <mateusmota0100@gmail.com>
2021-03-20 19:10:16 -0300

    Adicionando a resolução do exe03.

--- Listas de Atividades/Lista 05 - Operadores Condicionais/exercicio03.sh
+++ Listas de Atividades/Lista 05 - Operadores Condicionais/exercicio03.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 arq1=$(cat ${1} | wc -l)
 arq2=$(cat ${2} | wc -l)
-arq3=$(cat ${3} | wc -l)
-if [ $arq1 -gt $arq2 -a $arq1 -gt $arq3 ];then 
-       	echo $arq1
-elif [ $arq2 -gt $arq1 -a $arq2 -gt $arq3 ];then
-	echo $arq2
-elif [ $arq3 -gt $arq1 -a $arq3 -gt $arq2 ];then
-	echo $arq3
+if [ $arq1 -gt $arq2 ];then 
+       	echo $2
+elif [ $arq2 -gt $arq1 ];then
+	echo $1
 fi
