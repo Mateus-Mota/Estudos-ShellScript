0e3626b76fbb68f493bca01325c53a32b6363851
Mateus <mateusmota0100@gmail.com>
2021-03-20 02:12:45 -0300

    Readicionando o Módulo 02.

---
+++ Módulo 02 - Variaveis/exercicio02-base.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+read -p "Nome-1: " nome1
+read -p "Nome-2: " nome2
+read -p "Nome-3: " nome3
+
+read -p "Fone-1: " fone1
+read -p "Fone-2: " fone2
+read -p "Fone-3: " fone3
+
+echo "Nomes: 1 =${nome1}, 2 =${nome2}, 3 =${nome3}"
+echo "Fones: 1 =${fone1}, 2 =${fone2}, 3 =${fone3}"
---
+++ Módulo 02 - Variaveis/exercicio04-melhorado.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+a="Brasil"
+aa="Brasileiro"
+aaa="Brasileirissimo"
+aaaa="Braisleirossimozinho"
+
+echo $a
+echo $aa
+echo $aaa
+echo $aaaa
+
+echo $a''$aa
+echo $a''$aa'a'
