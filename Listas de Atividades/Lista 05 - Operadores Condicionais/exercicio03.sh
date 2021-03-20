c7a2502ae0e9bf43b3a2ba575671b57c140c0e37
Mateus_Mota <47959481+Mateus-Mota@users.noreply.github.com>
2021-03-20 18:44:49 -0300

    Update exercicio03.sh
    Adicionado a resolução da atividade.

--- Listas de Atividades/Lista 05 - Operadores Condicionais/exercicio03.sh
+++ Listas de Atividades/Lista 05 - Operadores Condicionais/exercicio03.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+arq1=$(cat ${1} | wc -l)
+arq2=$(cat ${2} | wc -l)
+arq3=$(cat ${3} | wc -l)
+if [ $arq1 -gt $arq2 -a $arq1 -gt $arq3 ];then 
+       	echo $arq1
+elif [ $arq2 -gt $arq1 -a $arq2 -gt $arq3 ];then
+	echo $arq2
+elif [ $arq3 -gt $arq1 -a $arq3 -gt $arq2 ];then
+	echo $arq3
+fi
