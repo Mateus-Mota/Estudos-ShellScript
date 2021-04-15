41f95ff4fbcaac943dc3fee1994e1bd889ee1e9b
Mateus <mateusmota0100@gmail.com>
2021-04-15 11:22:09 -0300

    Adicionado resolução da questão 01

--- Listas de Atividades/Lista 08 - Estruturas While e Case/exercicio01.sh
+++ Listas de Atividades/Lista 08 - Estruturas While e Case/exercicio01.sh
@@ -1,1 +1,30 @@
 #/bin/bash
+
+while [ "${opcao}" != sair ]; do
+	read -p "Digite a opção: " opcao
+
+	if [ "${opcao}" == "logica" ]; then
+		echo "logica"
+	fi
+
+	if [ "${opcao}" == "aritmetica" ];then
+		echo "arimetica"
+	fi
+
+	if [ "${opcao}" == "strings" ]; then
+		echo "String"
+	fi
+
+	if [ "${opcao}" == "variaveis" ]; then
+		echo "Variaveis"
+	fi
+
+	if [ "${opcao}" == "arquivos" ]; then
+		echo "arquivos"
+	fi
+
+	if [ "${opcao}" == "extend" ]; then
+		echo "extend"
+	fi
+
+done
