e4d274384300d114c2c6d2719954e95c45acc529
Mateus_Mota <47959481+Mateus-Mota@users.noreply.github.com>
2021-04-15 14:25:46 -0300

    Update exercicio03.sh
    Adicionado o menu da questão 03.

--- Listas de Atividades/Lista 08 - Estruturas While e Case/exercicio03.sh
+++ Listas de Atividades/Lista 08 - Estruturas While e Case/exercicio03.sh
@@ -1,1 +1,8 @@
 #/bin/bash
+
+# MENU DE OPÇÕES #
+# d) Para listar os diretórios no diretório atual
+# f) Para listar os arquivos do diretório atual
+# v) Para exibir o conteúdo de um arquivo
+# cd) Para entrar no diretório escolhido caso ele exista
+# q) Para Sair
