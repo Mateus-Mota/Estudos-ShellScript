7919b5e46405a053153d674030a4197f67adeef7
Mateus <mateusmota0100@gmail.com>
2021-04-27 12:44:03 -0300

    Adicionando o exercicio 01 resolvido.

--- Listas de Atividades/Lista 09 - Processamento de Texto/exercicio01.sh
+++ Listas de Atividades/Lista 09 - Processamento de Texto/exercicio01.sh
@@ -4,11 +4,24 @@ arquivo=$1
 numero=$2
 counter=0
 
-while read line; do
-	
-	counter=$(($counter + 1 ))
-	if (( $counter == $numero )); then
-	echo -e "$line"
-	fi
+if [ ! -f $arquivo ]; then
+	echo "Arquivo não existe"
+fi
 
-done < "$arquivo"
+if (( $(cat $arquivo | wc -l) < $numero )); then
+	echo "O arquivo só possui $(cat $arquivo | wc -l) linhas"
+fi
+
+if (( $(cat $arquivo | wc -l) >= $numero )) ;then
+
+	while read line; do
+	
+		counter=$(($counter + 1 ))
+		if (( $counter == $numero )); then
+			echo -e "$line"
+		fi
+
+	done < "$arquivo"
+
+fi
+
