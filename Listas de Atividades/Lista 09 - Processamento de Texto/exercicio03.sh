dafe4f3ca4f42981085b036e5223195b963f4594
Mateus_Mota <47959481+Mateus-Mota@users.noreply.github.com>
2021-04-28 00:53:22 -0300

    Update exercicio03.sh
    Adicionado leitura de arquivo por linha.

--- Listas de Atividades/Lista 09 - Processamento de Texto/exercicio03.sh
+++ Listas de Atividades/Lista 09 - Processamento de Texto/exercicio03.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 
+if (( $(cat $arquivo | wc -l) >= $numero )) ;then
+
+	while read line; do
+	
+		counter=$(($counter + 1 ))
+		if (( $counter == $numero )); then
+			echo -e "$line"
+		fi
+
+	done < "$arquivo"
