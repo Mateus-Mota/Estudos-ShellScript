70be6bdd4dd87373967e5ca4173073841468d3e4
Mateus <mateusmota0100@gmail.com>
2021-05-07 00:17:23 -0300

    Modificação o comando grep para egrep

--- Listas de Atividades/Lista 10 - Grep e RegEx/exercicio01.sh
+++ Listas de Atividades/Lista 10 - Grep e RegEx/exercicio01.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 if [ "$1" == "a" ]; then
-	ls -l | grep "^d" | cut -d ' ' -f 10
+	ls -l | egrep "^d" | cut -d ' ' -f 10
 fi
 
 if [ "$1" == "b" ]; then
-	ls -l | grep "^...x..[-x]..[-x]" | cut -d ' ' -f 10
+	ls -l | egrep "^...x..[-x]..[-x]" | cut -d ' ' -f 10
 fi
 
 if [ "$1" == "c" ]; then
-	ls | grep ".sh"
+	ls | egrep ".sh"
 fi
 
 if [ "$1" == "d" ]; then
-	ls -ls | grep "[-][>]" | cut -d ' ' -f 14 
+	ls -ls | egrep "[-][>]" | cut -d ' ' -f 14 
 fi
 
 if [ "$1" == "e" ]; then
-	ls | grep ""
+	ls | egrep ""
 fi
