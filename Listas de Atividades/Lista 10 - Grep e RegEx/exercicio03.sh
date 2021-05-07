1969f2d56eb27bab6f650563d31518f507a68dd4
Mateus <mateusmota0100@gmail.com>
2021-05-07 00:44:39 -0300

    Retirado a opção --color

--- Listas de Atividades/Lista 10 - Grep e RegEx/exercicio03.sh
+++ Listas de Atividades/Lista 10 - Grep e RegEx/exercicio03.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-read -p "Senha: " senha && echo $senha | egrep --color '*[0-9]*[A-Z]*[a-z]*|*[0-9]*[a-z]*[A-Z]*|*[A-Z]*[a-a]*[0-9]*|*[a-z]*[A-Z]*[0-9]*' && echo "SIM" || echo "NÃO"
+read -p "Senha: " senha && echo $senha | egrep '*[0-9]*[A-Z]*[a-z]*|*[0-9]*[a-z]*[A-Z]*|*[A-Z]*[a-a]*[0-9]*|*[a-z]*[A-Z]*[0-9]*' && echo "SIM" || echo "NÃO"
