7bd5ae164b03ac6f5e8acbc0af3ade4ee65ecf0c
Mateus <mateusmota0100@gmail.com>
2021-05-13 14:12:58 -0300

    Adicionado resolução da questão 03.

---
+++ Listas de Atividades/Lista 11 - Sed e RegEx/modificador-letras-digitos.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo -e "Digite uma das opções\n a - Remova todas as letras de um arquivo\n b - Remova todos os dígitos de um arquivo\n c - SUbstitua todos os caracteres que não nem caracteres nem dígitos por <?>"
+read -p "Opcão: " opcao
+
+if [ "$opcao" == "a" ]; then
+	sed 's/[[:alpha:]]\+//g' $1 
+fi
+
+if [ "$opcao" == "b" ]; then
+	sed 's/[[:digit:]]\+//g' $1
+fi
+
+if [ "$opcao" == "c" ]; then
+	sed '!s/[[:alpha:]]|[[:digit:]]/<?>/g' $1
+fi
